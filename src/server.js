2923bc80449ebec8067761a755fd02557a33f981
Utkarsh <3107utkarshpathak@gmail.com>
2025-08-06 16:23:38 +0530

    Backend for Notes App

---
+++ src/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js"
+import {connectDB} from "./config/db.js"
+import dotenv from "dotenv"
+import rateLimiter from "./middleware/rateLimiter.js"
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001
+connectDB();
+// middleware
+app.use(express.json())
+app.use(rateLimiter);
+//our simple custom middleware
+//app.use((req,res,next)=>{
+//console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//next();
+//})
+
+app.use("/api/notes",notesRoutes)
+
+app.listen(PORT, () => {
+console.log("Server started on PORT:",PORT)
+});
+
+
+//app.get("/api/notes", (req,res)=>{
+//res.status(200).send("you got 10 notes")
+//});
+//
+//app.post("/api/notes",(req,res)=>{
+//res.status(201).json({message:"Note created successfully"})
+//})
+//
+//app.put("/api/notes/:id",(req,res)=>{
+//res.status(200).json({message:"Note updated successfully"})
+//}) // for update we use put method
+//
+//app.delete("/api/notes/:id",(req,res)=>{
+//res.status(200).json({message:"Note deleted successfully"})
+//})
+
+
+
+
+//mongodb+srv://3107utkarshpathak:kWZF6Ky1DpR41Skh@cluster0.wqhtiub.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
